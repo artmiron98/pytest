03e3754ed5a30396009ecceef0d4b36d9ebf0d38
Artem Mironenkov <mir.art1998@gmail.com>
2023-08-03 20:26:51 +0500

    added new tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -3,7 +3,7 @@ import  pytest
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
-    #assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], -2, "test") == "test"
 
 def test_get__arrs_zero():
     with pytest.raises(IndexError):
@@ -12,3 +12,7 @@ def test_get__arrs_zero():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 1) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7], -3, -2) == [5]
+    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7], -8, 2) == [1, 2]
+
